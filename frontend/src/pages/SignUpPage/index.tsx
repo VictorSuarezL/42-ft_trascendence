9db7aee58e5cf9f1c08f87994b88a45623171db6
francfer-art <iwbtfmimf@gmail.com>
2026-08-24 20:39:39 +0200

    nginx + make lan

--- frontend/src/pages/SignUpPage/index.tsx
+++ frontend/src/pages/SignUpPage/index.tsx
@@ -16,7 +16,7 @@ export function SignUpPage() {
     }
 
     try {
-      const response = await fetch('http://localhost:3000/signup', {
+      const response = await fetch('/api/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
